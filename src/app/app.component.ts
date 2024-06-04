c285f5c71ef03f8bbab48a9ceefecadb7a4a95dd
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-06-04 12:41:59 +0200

    added atf section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, AboveTheFoldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
