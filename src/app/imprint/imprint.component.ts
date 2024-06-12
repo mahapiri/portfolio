9ebb6a08f63f0587ed453ad8340c7e18d0e3a75c
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-06-12 20:53:20 +0200

    edited back home + error (router)

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
-
+  hover: boolean = false;
 }
--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ShadowComponent } from '../shadow/shadow.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [ShadowComponent],
+  imports: [ShadowComponent, CommonModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
-
+  hover: boolean = false;
 }
