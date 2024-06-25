07c107196ad428d4643dde3a92fb8edb10498e0a
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-06-25 22:18:46 +0200

    edited animation on scroll

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
@@ -7,6 +7,34 @@ import { Component } from '@angular/core';
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss', './about-me-mobile.component.scss']
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit {
+  isVisible: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+      const aboutMeSection = document.getElementById('about-me');
+      if (aboutMeSection) {
+          const bounding = aboutMeSection.getBoundingClientRect();
+          const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+          // console.log('Bounding Rect:', bounding);
+          // console.log('Window Height:', windowHeight);
+
+          if (bounding.top < windowHeight && bounding.bottom > 0) {
+              this.isVisible = true;
+              console.log('Element is visible');
+          } else {
+              this.isVisible = false;
+              // console.log('Element is not visible');
+          }
+      }
+  }
+
+  ngOnInit(): void {
+      // Use a timeout to ensure that the element is rendered before checking visibility
+      setTimeout(() => {
+          this.onWindowScroll(); // Check visibility on init
+      }, 100);
+  }
 
 }
