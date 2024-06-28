9004a11f374fe375a53c42b2108dcbb207c30d72
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-06-28 15:21:29 +0200

    added shadows to all components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from '../above-the-fold/above-the-fold.component';
-import { ShadowComponent } from '../shadow/shadow.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { MySkillsComponent } from '../my-skills/my-skills.component';
 import { PortfolioComponent } from '../portfolio/portfolio.component';
@@ -10,7 +9,7 @@ import { LanguageService } from '../services/language/language.service';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboveTheFoldComponent, ShadowComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent],
+  imports: [AboveTheFoldComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ShadowComponent } from '../shadow/shadow.component';
 import { CommonModule } from '@angular/common';
 import { LanguageService } from '../services/language/language.service';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [ShadowComponent, CommonModule],
+  imports: [CommonModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
