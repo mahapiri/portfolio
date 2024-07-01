35bf34488acce6282e4161b86453c09db684fb4d
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-07-01 21:13:32 +0200

    edited

--- src/app/mail/mail.component.ts
+++ src/app/mail/mail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LanguageService } from '../services/language/language.service';
 
 @Component({
   selector: 'app-mail',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './mail.component.html',
   styleUrls: ['./mail.component.scss', './mail-mobile.component.scss']
 })
-export class MailComponent {
+export class MailComponent implements OnInit {
+  currentLanguage: string;
 
+  constructor(public languageService: LanguageService) {
+    this.currentLanguage = this.languageService.getLanguage();
+    this.languageService.language = this.currentLanguage;
+  }
+
+  ngOnInit(): void {
+
+  }
 }
