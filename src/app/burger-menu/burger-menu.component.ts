f7362e3c0984d31d9c19ee44ed2ebc98dee14bd8
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-07-09 13:17:16 +0200

    edited burger menu added language section

--- src/app/burger-menu/burger-menu.component.ts
+++ src/app/burger-menu/burger-menu.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { LanguageService } from '../services/language/language.service';
 
 @Component({
@@ -9,12 +9,22 @@ import { LanguageService } from '../services/language/language.service';
   templateUrl: './burger-menu.component.html',
   styleUrl: './burger-menu.component.scss'
 })
-export class BurgerMenuComponent {
+export class BurgerMenuComponent implements OnInit {
   @Output() linkVisited: EventEmitter<string> = new EventEmitter<string>();
 
   activeLink: string | null = null;
+  clickedMenu: boolean = false;
+  clickedLanguage: boolean = false;
 
   constructor(public languageService: LanguageService) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  openLanguage() {
+    this.clickedLanguage = !this.clickedLanguage;
+  }
 
   visited(id: string) {
     this.activeLink = id;
